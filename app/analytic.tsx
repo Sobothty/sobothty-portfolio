07f7fab76a9d15c3eaca17431176fac8155c7d95
Sobothty <supercrisz24@gmail.com>
2025-07-25 09:54:52 +0700

    update

--- app/analytic.tsx
+++ app/analytic.tsx
@@ -1,13 +1,24 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 import * as gtag from "@/lib/gtag";
-export function Analytic() {
+
+function AnalyticComponent() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
+
   useEffect(() => {
     const url = pathname + searchParams.toString();
     gtag.pageview(url);
   }, [pathname, searchParams]);
+
   return null;
+}
+
+export function Analytic() {
+  return (
+    <Suspense fallback={null}>
+      <AnalyticComponent />
+    </Suspense>
+  );
 }
